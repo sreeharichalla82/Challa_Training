c1af0f5d95dd4739e46c7feaa5cfe7acb10ac332
Vidya Challa <sreeharichalla82@gmail.com>
2023-10-28 16:17:16 -0500

    easy

---
+++ src/test/java/FirstProgram.java
@@ -0,0 +1,11 @@
+
+public class FirstProgram {
+// a class is nothing but an entity which consists of objects, variable, methods etc.
+	public static void main(String[] args) {
+		System.out.println("Hi Sree");
+
+		System.out.print("sree");
+		System.out.print("sree");
+		
+	}
+}
